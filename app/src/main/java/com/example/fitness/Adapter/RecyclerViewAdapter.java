ef3478f8ebf6da1f9d41e422eac7a1cfea6efe36
mervinkogei <vincent.kogei@yahoo.com>
2020-01-20 15:15:37 +0300

    Create RecyclerView Adapter and implement View OnclickListener

---
+++ app/src/main/java/com/example/fitness/Adapter/RecyclerViewAdapter.java
@@ -0,0 +1,20 @@
+package com.example.fitness.Adapter;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+class RecyclerViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+    public RecyclerViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
+public class RecyclerViewAdapter {
+}
